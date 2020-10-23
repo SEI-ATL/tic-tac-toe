e5eb21e9100b92c226ff743ddde8c6aca948a296
delayedaa <delayedaa@gmail.com>
2020-10-23 17:22:50 -0500

    [app.js] Build initial gameboard and add functionality to select X's and O's

--- js/app.js
+++ js/app.js
@@ -1,1 +1,19 @@
 console.log('Hello frontend');
+
+let gameboard = document.querySelector('.gameboard');
+
+gameboard.addEventListener('click', turnSquareRed);
+
+let player1 = true;
+
+function turnSquareRed(event) {
+  if (player1 === true) {
+    event.target.style.backgroundColor = 'green';
+    event.target.innerText = 'X';
+    player1 = false;
+  } else {
+    event.target.style.backgroundColor = 'green';
+    event.target.innerText = 'O';
+    player1 = true;
+  }
+}
