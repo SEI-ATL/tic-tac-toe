1a3214c26cd4496dbc3ae37eb9169c4663b72535
Your Name <you@example.com>
2020-10-23 20:37:05 -0400

    basic box and working x function

--- js/app.js
+++ js/app.js
@@ -1,1 +1,32 @@
-console.log('Hello frontend');
+function setX(spaceNum){
+    let newValue = document.getElementById(1).innerText = "X";
+    return newValue
+}
+
+setX(1)
+
+// console.log(document.getElementById(1))
+
+
+
+
+
+
+// have two functions set to change a paragraph to x or y
+// maybe have a radio button to denote which character you want to put
+// style it as a grid just ad a border on the grids to give it that box makeup
+// make a heading to tell us who's turn it is
+// detect winner and whether the game is a draw
+// maybe set the ids of the ps to 1-9 and use if statements to determine if someone has won after each turn
+
+
+//*BONUS set a clear button that will clear the board
+
+
+
+
+
+// DONE ____________________________________________
+// set up html like p,p,p paragraph elements
+// set up html like p,p,p paragraph elements
+// set up html like p,p,p paragraph elements
