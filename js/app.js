f3beca7197ba9710e0641f233d7b9079b962e392
Your Name <you@example.com>
2020-10-24 12:29:21 -0400

    [app.js] added clickable buttons for x

--- js/app.js
+++ js/app.js
@@ -1,71 +1,81 @@
-const winCond1 = [
+const winCondX1 = [
     document.getElementById("1").innerText === "x",
     document.getElementById("2").innerText === "x",
     document.getElementById("3").innerText === "x",
     // once finished make a condition to check (if true true true value in the trues wins)
     // use a for loop for every item in list if one is false there is no winner
 ]
-const winCond2 = [
+const winCondX2 = [
     document.getElementById("4").innerText === "x",
     document.getElementById("5").innerText === "x",
     document.getElementById("6").innerText === "x",
     // once finished make a condition to check (if true true true value in the trues wins)
     // use a for loop for every item in list if one is false there is no winner
 ]
-const winCond3 = [
+const winCondX3 = [
     document.getElementById("7").innerText === "x",
     document.getElementById("8").innerText === "x",
     document.getElementById("9").innerText === "x",
     // once finished make a condition to check (if true true true value in the trues wins)
     // use a for loop for every item in list if one is false there is no winner
 ]
-const winCond4 = [
+const winCondX4 = [
     document.getElementById("1").innerText === "x",
     document.getElementById("4").innerText === "x",
     document.getElementById("7").innerText === "x",
     // once finished make a condition to check (if true true true value in the trues wins)
     // use a for loop for every item in list if one is false there is no winner
 ]
-const winCond5 = [
+const winCondX5 = [
     document.getElementById("2").innerText === "x",
     document.getElementById("5").innerText === "x",
     document.getElementById("8").innerText === "x",
     // once finished make a condition to check (if true true true value in the trues wins)
     // use a for loop for every item in list if one is false there is no winner
 ]
-const winCond6 = [
+const winCondX6 = [
     document.getElementById("3").innerText === "x",
     document.getElementById("6").innerText === "x",
     document.getElementById("9").innerText === "x",
     // once finished make a condition to check (if true true true value in the trues wins)
     // use a for loop for every item in list if one is false there is no winner
 ]
-const winCond7 = [
+const winCondX7 = [
     document.getElementById("1").innerText === "x",
     document.getElementById("5").innerText === "x",
     document.getElementById("9").innerText === "x",
     // once finished make a condition to check (if true true true value in the trues wins)
     // use a for loop for every item in list if one is false there is no winner
 ]
-const winCond8 = [
+const winCondX8 = [
     document.getElementById("3").innerText === "x",
     document.getElementById("5").innerText === "x",
     document.getElementById("7").innerText === "x",
     // once finished make a condition to check (if true true true value in the trues wins)
     // use a for loop for every item in list if one is false there is no winner
 ]
-function setX(spaceNum){
-    let newValue = document.getElementById(spaceNum).innerText = "x";
-    return newValue
+
+function setX(click){
+    document.getElementById(click).innerText = "x";
+    console.log(click)
+
 }
+
+function click(event,target, id){
+    document.addEventListener('click', function(e){
+        setX(e.target.id)
+    }
+    
+    )
+}
+click()
+setX(1)
 
 function setY(spaceNum){
     let newValue = document.getElementById(spaceNum).innerText = "o";
     return newValue
 }
 
-
-// console.log(document.getElementById(1))
 
 const winCondsX = [winCond1, winCond2, winCond3,  winCond4,  winCond5,  winCond6,  winCond7,  winCond8]
 
@@ -89,12 +99,14 @@ function checkBoard(){ for (let i = 0; i <= 8; i++) {
 
 }
 
-
 let winValue = [true, true, true]
-console.log(winValue === winCond1)
+
+
+
 console.log(winCond1)
 console.log(winValue)
-
+console.log(winValue === winCond1)
+// make a loop to check index of each winCond and winValue for the same value
 // detect winner and whether the game is a draw ** WIN CONDITIONS MADE HOWEVER NEEDS IF STATEMENT ALSO NEEDS A FUNCTION THAT CHECKS FOR DRAW
 // maybe set the ids of the ps to 1-9 and use if statements to determine if someone has won after each turn
 // maybe use something to check if every square is no longer a number and no one has won then the game is a draw
