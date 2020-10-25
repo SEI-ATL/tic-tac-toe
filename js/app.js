6bceebac8e1f4e07c3a9d6bad95fe59e4af15b44
Jeremy Uriz <jeremyuriz@Jennifers-Air.attlocal.net>
2020-10-24 21:47:56 -0400

    [app] Completely lost. Starting over.

--- js/app.js
+++ js/app.js
@@ -1,28 +1,14 @@
 'use strict';
 
-// Variable Declarations
-let board = ["", "", "", "", "", "", "", "", ""];
-let activeBoard = board;
-
-const boxes = Array.from(document.querySelectorAll('#boxes div')); // Can I use .box here? Need to test.
-
-const winArray = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9],
+// List of winning combinations in the game. 
+const winningArray = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
     [1, 4, 7],
     [2, 5, 8],
-    [3, 6, 9],
-    [1, 5, 9],
-    [3, 5, 7]
+    [0, 4, 8],
+    [2, 4, 6]
 ]; 
-
-function render() {
-    activeBoard.forEach((value, index) => {
-        console.log(value, index);
-        boxes[index].textContent = value;
-    });
-};
-
-render();
 
