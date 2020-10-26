40a0e096384189eee178b8aa7d24a4b385ed927a
Angel's Work Station (Coding) <angelsworkstation@Angels-iMac.local>
2020-10-25 20:10:25 -0700

    [js] Board game setup with basics using HTML and CSS and a little of javascript.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
-console.log('Hello frontend');
+const topLeft = document.querySelector('#topLeft').addEventListener("click", playerOneMove)
+const topMiddle = document.querySelector('#topMiddle').addEventListener("click", playerTwoMove)
+const topRight = document.querySelector('#topRight')
+const middleLeft = document.querySelector('#middleLeft')
+const center = document.querySelector('#center')
+const middleRight = document.querySelector('#middleRight')
+const bottomLeft = document.querySelector('#bottomLeft')
+const bottomMiddle = document.querySelector('#bottomMiddle')
+const bottomRight = document.querySelector('#bottomRight')
+
+function playerOneMove() {
+    document.getElementById("topLeft").innerHTML = "X"
+};
+function playerTwoMove() {
+    document.getElementById("topMiddle").innerHTML = "O"
+};
+
