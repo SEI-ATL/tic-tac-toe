ffdc7adbef5e3e7c2bae1c49ad1e65763fd52ed8
Angel's Work Station (Coding) <angelsworkstation@Angels-iMac.local>
2020-10-25 23:28:27 -0700

    [add structure and css to game, as well as target dom elements with click

--- .github/PULL_REQUEST_TEMPLATE.md
+++ .github/PULL_REQUEST_TEMPLATE.md
@@ -2,8 +2,8 @@
 
 Please describe your comfort and completeness levels before submitting.
 
-Comfort Level (1-5): 
+Comfort Level (1-5): 3
 
-Completeness Level (1-5):
+Completeness Level (1-5): 3
 
-What did you think of this deliverable?:
+What did you think of this deliverable?: I'm starting to get more used to CSS and definitely HTML. I could've made it look way better if i am being honest but I wanted to get the functionality and basics first before polishing it. In the end this is all I could do . Part of my brain undertands the concept but the other part gave up on me. I will probably try to look for more DOM elements and function practice to help me out.
--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
-const topLeft = document.querySelector('#topLeft').addEventListener("click", playerOneMove)
-const topMiddle = document.querySelector('#topMiddle').addEventListener("click", playerTwoMove)
+// DOM elements
+const topLeft = document.querySelector('#topLeft')
+const topMiddle = document.querySelector('#topMiddle')
 const topRight = document.querySelector('#topRight')
 const middleLeft = document.querySelector('#middleLeft')
 const center = document.querySelector('#center')
@@ -7,11 +8,34 @@ const middleRight = document.querySelector('#middleRight')
 const bottomLeft = document.querySelector('#bottomLeft')
 const bottomMiddle = document.querySelector('#bottomMiddle')
 const bottomRight = document.querySelector('#bottomRight')
+const startGame = document.querySelector('.start')
+const resetGame = document.querySelector('.reset')
 
-function playerOneMove() {
-    document.getElementById("topLeft").innerHTML = "X"
-};
-function playerTwoMove() {
-    document.getElementById("topMiddle").innerHTML = "O"
-};
+// event listeners
+
+topLeft.addEventListener("click", )
+topMiddle.addEventListener("click", )
+topRight.addEventListener("click", )
+middleLeft.addEventListener("click", )
+center.addEventListener("click", )
+middleRight.addEventListener("click", )
+bottomLeft.addEventListener("click", )
+bottomMiddle.addEventListener("click", )
+bottomRight.addEventListener("click", )
+
+
+function makeMove() {
+
+}
+
+
+
+// testing to grab an element
+
+// function playerOneMove() {
+//     document.getElementById("topLeft").innerHTML = "X"
+// };
+// function playerTwoMove() {
+//     document.getElementById("topMiddle").innerHTML = "O"
+// };
 
